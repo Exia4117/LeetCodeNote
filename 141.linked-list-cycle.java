a19340f179a13b9272f2f10934719f3e59467969
Exia4117 <593663174.bz@gmail.com>
2019-12-17 21:39:02 +0800

    linked-list-cycle

---
+++ 141.linked-list-cycle.java
@@ -0,0 +1,105 @@
+import java.util.HashSet;
+
+/*
+ * @lc app=leetcode id=141 lang=java
+ *
+ * [141] Linked List Cycle
+ *
+ * https://leetcode.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (38.73%)
+ * Likes:    2039
+ * Dislikes: 298
+ * Total Accepted:    501.7K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ * 
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0
+ * Output: true
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1
+ * Output: false
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * Can you solve it using O(1) (i.e. constant) memory?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list. class ListNode { int val; ListNode next;
+ * ListNode(int x) { val = x; next = null; } }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return false;
+        }
+        // HashSet<ListNode> help = new HashSet<>();
+        // ListNode cur = head;
+        // while (cur != null) {
+        // if (help.contains(cur)) {
+        // return true;
+        // }
+        // help.add(cur);
+        // cur = cur.next;
+        // }
+        // return false;
+
+        ListNode fast = head.next.next;
+        ListNode slow = head.next;
+        while (fast != slow) {
+            if (fast.next == null || fast.next.next == null) {
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
+// @lc code=end
